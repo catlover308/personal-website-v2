e8c34a08ec2ab922f71e4e1a8dd05ca9d9a032d9
Bryce Lee <leebryce307@gmail.com>
2026-05-10 14:31:40 -0700

    chore: remove Cloudflare adapter and server-side rendering configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 import tailwindcss from '@tailwindcss/vite';
-
-// import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
-  // or 'hybrid'
-  output: 'server',
-
-  adapter: cloudflare({
-    mode: 'directory'
-  }),
 
   vite: {
     plugins: [tailwindcss()]
